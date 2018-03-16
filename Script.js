afb2305cd480fec8550fbe3795e0734a04fa6149
WSW <shengwei.wan@ceasia.com.cn>
2018-03-16 18:38:49 +0800

    update

--- Script.js
+++ Script.js
@@ -68,7 +68,8 @@ handlers.GetDailyBonus = function (args, context) {
                 data:{
                     hascheckin:true,
                     BonusCount:dailyInfo.BonusCount,
-                    LastCheckinTime:dailyInfo.LastCheckinTime
+                    LastCheckinTime:dailyInfo.LastCheckinTime,
+                    LevelList:dailyInfo.LevelList
                 }}
         }else if(checkonly){
             return{status:"ok",code:200,
@@ -76,12 +77,26 @@ handlers.GetDailyBonus = function (args, context) {
                     hascheckin:false
                 }}
         }
+
+        request = {
+            PlayFabId: currentPlayerId,
+            Keys: ["Challenges:V7.0"]
+        };
+        let challengesResult=server.GetUserReadOnlyData(request)
+        if(challengesResult.Data.hasOwnProperty("Challenges:V7.0")){
+            var challenges=JSON.parse(challengesResult.Data['Challenges:V7.0'].Value);
+            var level=challenges.Level
+        }else{
+            level=0
+        }
         if(dailyInfo.hasOwnProperty("LastCheckinTime")
         &&(today.getDay()==0?7:today.getDay())>lastCheckinTime.getDay()
         &&today.getDate()-lastCheckinTime.getDate()<7){
             dailyInfo.BonusCount+=1;
+            dailyInfo.LevelList.push(level)
         }else{
             dailyInfo.BonusCount=1;
+            dailyInfo.LevelList=[level]
         }
         dailyInfo.LastCheckinTime=Date.now()/1000;
         request = {
@@ -94,7 +109,8 @@ handlers.GetDailyBonus = function (args, context) {
         return {status:"ok",code:200,
             data:{
                 BonusCount:dailyInfo.BonusCount,
-                LastCheckinTime:dailyInfo.LastCheckinTime
+                LastCheckinTime:dailyInfo.LastCheckinTime,
+                LevelList:dailyInfo.LevelList
             }}
     }catch (ex) {
         log.error(ex);
@@ -243,58 +259,4 @@ handlers.unlockHighSkillContent = function (args, context) {
     var playerInternalData = server.UpdateUserInternalData(request);
     log.info('Unlocked HighSkillContent for ' + context.playerProfile.DisplayName);
     return { profile: context.playerProfile };
-};
-
-// Photon Webhooks Integration
-//
-// The following functions are examples of Photon Cloud Webhook handlers.
-// When you enable the Photon Add-on (https://playfab.com/marketplace/photon/)
-// in the Game Manager, your Photon applications are automatically configured
-// to authenticate players using their PlayFab accounts and to fire events that
-// trigger your Cloud Script Webhook handlers, if defined.
-// This makes it easier than ever to incorporate multiplayer server logic into your game.
-
-
-// Triggered automatically when a Photon room is first created
-handlers.RoomCreated = function (args) {
-    log.debug("Room Created - Game: " + args.GameId + " MaxPlayers: " + args.CreateOptions.MaxPlayers);
-};
-
-// Triggered automatically when a player joins a Photon room
-handlers.RoomJoined = function (args) {
-    log.debug("Room Joined - Game: " + args.GameId + " PlayFabId: " + args.UserId);
-};
-
-// Triggered automatically when a player leaves a Photon room
-handlers.RoomLeft = function (args) {
-    log.debug("Room Left - Game: " + args.GameId + " PlayFabId: " + args.UserId);
-};
-
-// Triggered automatically when a Photon room closes
-// Note: currentPlayerId is undefined in this function
-handlers.RoomClosed = function (args) {
-    log.debug("Room Closed - Game: " + args.GameId);
-};
-
-// Triggered automatically when a Photon room game property is updated.
-// Note: currentPlayerId is undefined in this function
-handlers.RoomPropertyUpdated = function (args) {
-    log.debug("Room Property Updated - Game: " + args.GameId);
-};
-
-// Triggered by calling "OpRaiseEvent" on the Photon client. The "args.Data" property is
-// set to the value of the "customEventContent" HashTable parameter, so you can use
-// it to pass in arbitrary data.
-handlers.RoomEventRaised = function (args) {
-    var eventData = args.Data;
-    log.debug("Event Raised - Game: " + args.GameId + " Event Type: " + eventData.eventType);
-
-    switch (eventData.eventType) {
-    case "playerMove":
-        processPlayerMove(eventData);
-        break;
-
-    default:
-        break;
-    }
 };
