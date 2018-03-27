7756a2ced8958eb6bbcc6a9447298977349dcbb8
WSW <shengwei.wan@ceasia.com.cn>
2018-03-27 14:38:08 +0800

    test

--- Script.js
+++ Script.js
@@ -42,10 +42,12 @@ function calcLevelReward(dailyRewards,dailyInfo,today,level) {
     for(let val of dailyRewards['SpecialDailyRewards']){
         if(val.hasOwnProperty('UseSpecialReward')&&val.UseSpecialReward&&val.hasOwnProperty("StartDate")){
             let startDate=new Date(val.StartDate.replace(/-/g,"/"));
-            log.info((startDate.getTime()+val.Duration*dayofms)>today.getTime())
             if(startDate.getTime()<today.getTime()&&(startDate.getTime()+val.Duration*dayofms)>today.getTime()){
+                log.info(1)
                 specialDailyReward=val;
+                log.info(specialDailyReward)
                 break;
+                log.info(2)
             }
         }
     }
