effd46e1e102dffaf74949daa40034a0f33cf722
WSW <shengwei.wan@ceasia.com.cn>
2018-03-27 11:31:22 +0800

    update

--- Script.js
+++ Script.js
@@ -33,6 +33,26 @@ handlers.Info = function (args, context) {
     }
 };
 
+function calcLevelReward(dailyRewards,dailyInfo,today) {
+    let dailyReward=dailyRewards[(dailyInfo.BonusCount-1).toString()];
+    let specialDailyReward=null;
+    let levelReward=null;
+    for( var val of dailyRewards['SpecialDailyRewards']){
+        if(val.UseSpecialReward&&val.hasOwnProperty("StartDate")){
+
+            let startDate=Date(val.StartDate)
+            log.info(startDate)
+            log.info(val.a)
+        }
+    }
+    for(var val of dailyReward){
+        if(val['StartLevel']<=level &&(levelReward==undefined ||val['StartLevel']>levelReward['StartLevel']) ){
+            levelReward=val;
+        }
+    }
+    return levelReward;
+}
+
 handlers.GetDailyBonus = function (args, context) {
     try{
         let checkonly=false;
@@ -55,7 +75,8 @@ handlers.GetDailyBonus = function (args, context) {
             var dailyInfo={
                 BonusCount:0,
                 RewardLevels:[],
-                LastCheckinTime:0
+                LastCheckinTime:0,
+                :0
             };
         }
         let couldCheckin=true;
@@ -72,7 +93,8 @@ handlers.GetDailyBonus = function (args, context) {
                     HasNew:false,
                     BonusCount:dailyInfo.BonusCount,
                     LastCheckinTime:dailyInfo.LastCheckinTime,
-                    RewardLevels:dailyInfo.RewardLevels
+                    RewardLevels:dailyInfo.RewardLevels,
+                    SpecialBonusCount:dailyInfo.SpecialBonusCount
                 }}
         }else if(checkonly){
             return{status:"ok",code:200,
@@ -104,6 +126,7 @@ handlers.GetDailyBonus = function (args, context) {
         }else{
             dailyInfo.BonusCount=1;
             dailyInfo.RewardLevels=[level]
+            dailyInfo.SpecialBonusCount=0
         }
         dailyInfo.LastCheckinTime=Date.now()/1000;
 
@@ -111,18 +134,16 @@ handlers.GetDailyBonus = function (args, context) {
             Keys: ["DailyRewards"]
         };
         let dailyRewardsResult=server.GetTitleData(request);
+        log.info(dailyRewardsResult,dailyInfo,today)
         if(!dailyRewardsResult.Data.hasOwnProperty("DailyRewards")){
             return {status:"reward not exist",code:500};
         }else{
-            var dailyReward=JSON.parse(dailyRewardsResult.Data.DailyRewards)[(dailyInfo.BonusCount-1).toString()];
-        }
-        var levelReward=undefined;
-        for(var val of dailyReward){
-            if(val['StartLevel']<=level &&(levelReward==undefined ||val['StartLevel']>levelReward['StartLevel']) ){
-                levelReward=val;
-            }
+            var levelReward=calcLevelReward(JSON.parse(dailyRewardsResult.Data.DailyRewards));
         }
         log.info(levelReward)
+        if(levelReward.hasOwnProperty("UseSpecialReward")&&levelReward.UsespecialReward){
+            dailyInfo.SpecialBonusCount+=1;
+        }
         /**********************begin to award **************************/
         let qdResID=null
         let itemInstanceId=null
@@ -161,6 +182,7 @@ handlers.GetDailyBonus = function (args, context) {
                 BonusCount:dailyInfo.BonusCount,
                 LastCheckinTime:dailyInfo.LastCheckinTime,
                 RewardLevels:dailyInfo.RewardLevels,
+                SpecialBonusCount:dailyInfo.SpecialBonusCount,
                 QDResID:qdResID,
                 ItemInstanceId:itemInstanceId
             }}
@@ -224,75 +246,6 @@ handlers.handlePlayStreamEventAndProfile = function (args, context) {
     return { externalAPIResponse: response };
 };
 
-
-// In addition to the Cloud Script handlers, you can define your own functions and call them from your handlers.
-// This makes it possible to share code between multiple handlers and to improve code organization.
-handlers.updatePlayerMove = function (args) {
-    var validMove = processPlayerMove(args);
-    return { validMove: validMove };
-};
-
-
-// This is a helper function that verifies that the player's move wasn't made
-// too quickly following their previous move, according to the rules of the game.
-// If the move is valid, then it updates the player's statistics and profile data.
-// This function is called from the "UpdatePlayerMove" handler above and also is
-// triggered by the "RoomEventRaised" Photon room event in the Webhook handler
-// below.
-//
-// For this example, the script defines the cooldown period (playerMoveCooldownInSeconds)
-// as 15 seconds. A recommended approach for values like this would be to create them in Title
-// Data, so that they can be queries in the script with a call to GetTitleData
-// (https://api.playfab.com/Documentation/Server/method/GetTitleData). This would allow you to
-// make adjustments to these values over time, without having to edit, test, and roll out an
-// updated script.
-function processPlayerMove(playerMove) {
-    var now = Date.now();
-    var playerMoveCooldownInSeconds = 15;
-
-    var playerData = server.GetUserInternalData({
-        PlayFabId: currentPlayerId,
-        Keys: ["last_move_timestamp"]
-    });
-
-    var lastMoveTimestampSetting = playerData.Data["last_move_timestamp"];
-
-    if (lastMoveTimestampSetting) {
-        var lastMoveTime = Date.parse(lastMoveTimestampSetting.Value);
-        var timeSinceLastMoveInSeconds = (now - lastMoveTime) / 1000;
-        log.debug("lastMoveTime: " + lastMoveTime + " now: " + now + " timeSinceLastMoveInSeconds: " + timeSinceLastMoveInSeconds);
-
-        if (timeSinceLastMoveInSeconds < playerMoveCooldownInSeconds) {
-            log.error("Invalid move - time since last move: " + timeSinceLastMoveInSeconds + "s less than minimum of " + playerMoveCooldownInSeconds + "s.");
-            return false;
-        }
-    }
-
-    var playerStats = server.GetPlayerStatistics({
-        PlayFabId: currentPlayerId
-    }).Statistics;
-    var movesMade = 0;
-    for (var i = 0; i < playerStats.length; i++)
-        if (playerStats[i].StatisticName === "")
-            movesMade = playerStats[i].Value;
-    movesMade += 1;
-    var request = {
-        PlayFabId: currentPlayerId, Statistics: [{
-            StatisticName: "movesMade",
-            Value: movesMade
-        }]
-    };
-    server.UpdatePlayerStatistics(request);
-    server.UpdateUserInternalData({
-        PlayFabId: currentPlayerId,
-        Data: {
-            last_move_timestamp: new Date(now).toUTCString(),
-            last_move: JSON.stringify(playerMove)
-        }
-    });
-
-    return true;
-}
 
 // This is an example of using PlayStream real-time segmentation to trigger
 // game logic based on player behavior. (https://playfab.com/introducing-playstream/)
