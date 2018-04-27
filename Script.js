392f763d2ff6386d4db22471d1353c7e9154a2d2
WSW <shengwei.wan@ceasia.com.cn>
2018-04-27 16:29:37 +0800

    del log

--- Script.js
+++ Script.js
@@ -42,9 +42,6 @@ function calcLevelReward(dailyRewards,day,today,level) {
     for(let val of dailyRewards['SpecialDailyRewards']){
         if(val.hasOwnProperty('UseSpecialReward')&&val.UseSpecialReward&&val.hasOwnProperty("StartDate")){
             let startDate=new Date(val.StartDate)
-            log.info(startDate.getTime())
-            log.info(today.getTime())
-            log.info(startDate.getTime()+val.Duration*dayofms)
             if(startDate.getTime()<today.getTime()&&(startDate.getTime()+val.Duration*dayofms)>today.getTime()){
                 specialDailyRewards=val;
                 break;
